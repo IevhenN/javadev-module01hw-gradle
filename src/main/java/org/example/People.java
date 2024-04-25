94090928e24eca12117bf0ca60add6e4c7b31a65
Ievhen <ievhennesterenko@gmail.com>
2024-04-25 19:53:21 +0300

    init commit

---
+++ src/main/java/org/example/People.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class People {
+    private String name;
+    private String lastName;
+
+    public People(String name, String lastName) {
+        this.name = name;
+        this.lastName = lastName;
+    }
+}
